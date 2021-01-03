3247bbcf753a59f14a8ccc699d2b8ceb871bdc36
Eran Cohen <eran@stratoscale.com>
2021-01-03 12:18:43 +0200

    Wait for kube-apiserver-operator to be ready before restarting kubelet

--- data/data/bootstrap/bootstrap-in-place/opt/openshift/bootstrap-in-place/bootstrap-in-place-post-reboot.sh
+++ data/data/bootstrap/bootstrap-in-place/opt/openshift/bootstrap-in-place/bootstrap-in-place-post-reboot.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -euoE pipefail ## -E option will cause functions to inherit trap
 
-
 export KUBECONFIG=/etc/kubernetes/bootstrap-secrets/kubeconfig
+
 function wait_for_api {
   until oc get csr &> /dev/null
   do
@@ -10,17 +10,26 @@ function wait_for_api {
       sleep 5
   done
 }
+
 function restart_kubelet {
   echo "Restarting kubelet"
-  while grep  bootstrap-kube-apiserver /etc/kubernetes/manifests/kube-apiserver-pod.yaml;
+  until [ "$(oc get pod -n openshift-kube-apiserver-operator --selector='app=kube-apiserver-operator'  -o jsonpath='{.items[0].status.conditions[?(@.type=="Ready")].status}' | grep -c "True")" -eq 1 ];
   do
-    echo "Waiting for kube-apiserver to apply the new static pod configuration"
+    echo "Waiting for kube-apiserver-operator to ready condition to be True"
     sleep 10
   done
   # daemon-reload is required because /etc/systemd/system/kubelet.service.d/20-nodenet.conf is added after kubelet started
   systemctl daemon-reload
   systemctl restart kubelet
+
+  while grep  bootstrap-kube-apiserver /etc/kubernetes/manifests/kube-apiserver-pod.yaml;
+  do
+    echo "Waiting for kube-apiserver to apply the new static pod configuration"
+    sleep 10
+  done
+  systemctl restart kubelet
 }
+
 function approve_csr {
   echo "Approving csrs ..."
   needed_to_approve=false
@@ -30,25 +39,28 @@ function approve_csr {
       echo "Approving csrs ..."
      oc get csr -o go-template='{{range .items}}{{if not .status}}{{.metadata.name}}{{"\n"}}{{end}}{{end}}' | xargs oc adm certificate approve &> /dev/null || true
      sleep 30
-    done
+  done
   # Restart kubelet only if node was added
   if $needed_to_approve ; then
     restart_kubelet
   fi
 }
+
 function wait_for_cvo {
   echo "Waiting for cvo"
   until [ "$(oc get clusterversion -o jsonpath='{.items[0].status.conditions[?(@.type=="Available")].status}')" == "True" ];
   do
     echo "Still waiting for cvo ..."
     sleep 30
-   done
+  done
 }
+
 function clean {
   if [ -d "/etc/kubernetes/bootstrap-secrets" ]; then
      rm -rf /etc/kubernetes/bootstrap-*
   fi
 }
+
 wait_for_api
 approve_csr
 wait_for_cvo
