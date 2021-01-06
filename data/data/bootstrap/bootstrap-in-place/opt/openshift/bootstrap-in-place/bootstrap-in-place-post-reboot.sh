58d0722ca0f5b6f81146a673d0a8ce3c90318fe2
Igal Tsoiref <itsoiref@redhat.com>
2021-01-06 13:24:58 +0200

    Move patch to after pivot

--- data/data/bootstrap/bootstrap-in-place/opt/openshift/bootstrap-in-place/bootstrap-in-place-post-reboot.sh
+++ data/data/bootstrap/bootstrap-in-place/opt/openshift/bootstrap-in-place/bootstrap-in-place-post-reboot.sh
@@ -42,17 +42,53 @@ function approve_csr {
   done
   # Restart kubelet only if node was added
   if $needed_to_approve ; then
+    apply_patches pre_kubelet_restart_patches
     restart_kubelet
   fi
 }
 
-function patch_ingress {
-  echo "patch ingress operator to run a single router pod"
-  while ! oc patch -n openshift-ingress-operator ingresscontroller/default --patch '{"spec":{"replicas": 1}}' --type=merge;
-  do
-    echo "Still waiting to patch ingress ..."
-    sleep 3
-  done
+function pre_kubelet_restart_patches {
+    echo "patch etcd to allow etcd-operator to start the etcd cluster without minimum of 3 master nodes"
+    oc patch etcd cluster -p='{"spec": {"unsupportedConfigOverrides": {"useUnsupportedUnsafeNonHANonProductionUnstableEtcd": true}}}' --type=merge || return 1
+
+    echo "patch cluster-authentication-operator to allow to deploy OAuthServer without minimum of 3 master nodes"
+    oc patch authentications.operator.openshift.io cluster -p='{"spec": {"managementState": "Managed", "unsupportedConfigOverrides": {"useUnsupportedUnsafeNonHANonProductionUnstableOAuthServer": true}}}' --type=merge || return 1
+
+    return 0
+}
+
+function post_kubelet_restart_patches {
+
+    echo "patch ingress operator to run a single router pod"
+    oc patch -n openshift-ingress-operator ingresscontroller/default --patch '{"spec":{"replicas": 1}}' --type=merge || return 1
+
+
+    if oc get deployment etcd-quorum-guard -n openshift-etcd; then
+      echo "mark etcd-quorum-guard as unmanaged"
+      oc patch clusterversion/version --type='merge' -p "$(cat <<- EOF
+spec:
+    overrides:
+      - group: apps/v1
+        kind: Deployment
+        name: etcd-quorum-guard
+        namespace: openshift-etcd
+        unmanaged: true
+EOF
+  )" || return 1
+
+      echo "scale down etcd-quorum-guard"
+      oc scale --replicas=0 deployment/etcd-quorum-guard -n openshift-etcd || return 1
+   fi
+
+   return 0
+}
+
+function apply_patches {
+  echo "patch needed components"
+  while ! $1; do
+    echo "Waiting to try again..."
+    sleep 10
+done
 }
 
 function wait_for_cvo {
@@ -73,6 +109,6 @@ function clean {
 
 wait_for_api
 approve_csr
-patch_ingress
+apply_patches post_kubelet_restart_patches
 wait_for_cvo
 clean
